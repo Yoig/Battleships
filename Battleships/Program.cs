a09a7547431e93571819b4329392140605741be1
Yoig <obajt130199@gmail.com>
2020-07-10 00:17:51 +0200

    Add simple test view

--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -8,15 +8,18 @@ namespace Battleships
         private static void Main(string[] args)
         {
             var menu = new Menu();
+            var view = new View();
 
             var option = Input.Read();
             switch (Input.GetOptionType(option))
             {
                 case Input.OptionType.Menu:
                     Console.WriteLine("Chosen menu option");
+                    view.Update();
                     break;
                 case Input.OptionType.Game:
                     Console.WriteLine("Chosen game option");
+                    view.Update();
                     break;
                 case Input.OptionType.Error:
                     Console.WriteLine("Error");
--- ConsoleManagement/View.cs
+++ ConsoleManagement/View.cs
@@ -1,7 +1,17 @@
-﻿namespace ConsoleManagement
+﻿using System;
+
+namespace ConsoleManagement
 {
     public class View
     {
-        
+        public void Update()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
